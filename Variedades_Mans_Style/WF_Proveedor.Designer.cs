231fcc421e4ea3eb3501071dd65760ec6004d66e
DevSaile <jeserelias12@gmail.com>
2024-11-10 19:59:15 -0600

    pull a cambio de urbina

---
+++ Variedades_Mans_Style/WF_Proveedor.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Variedades_Man_s_Style
+{
+    partial class WF_Proveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WF_Proveedor));
+            this.Panel_txtProveedor = new System.Windows.Forms.Panel();
+            this.Label_Proveedores = new System.Windows.Forms.Label();
+            this.btn_WF_CerrarProveedor = new System.Windows.Forms.Button();
+            this.btn_WF_Eliminar = new System.Windows.Forms.Button();
+            this.btn_WF_Actualizar = new System.Windows.Forms.Button();
+            this.btn_WF_Agregar = new System.Windows.Forms.Button();
+            this.btn_WF_Buscar = new System.Windows.Forms.Button();
+            this.Panel_WF_Controles_Proveedor = new System.Windows.Forms.Panel();
+            this.PicBox_WF_Proveedor = new System.Windows.Forms.PictureBox();
+            this.Panel_WF_Info_Proveedor = new System.Windows.Forms.Panel();
+            this.txt_Busqueda_Proveedor = new System.Windows.Forms.TextBox();
+            this.DGV_WF_Proveedor = new System.Windows.Forms.DataGridView();
+            this.Panel_txtProveedor.SuspendLayout();
+            this.Panel_WF_Controles_Proveedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_WF_Proveedor)).BeginInit();
+            this.Panel_WF_Info_Proveedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_WF_Proveedor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel_txtProveedor
+            // 
+            this.Panel_txtProveedor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel_txtProveedor.BackColor = System.Drawing.Color.Navy;
+            this.Panel_txtProveedor.Controls.Add(this.Label_Proveedores);
+            this.Panel_txtProveedor.Controls.Add(this.btn_WF_CerrarProveedor);
+            this.Panel_txtProveedor.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Panel_txtProveedor.Location = new System.Drawing.Point(0, 0);
+            this.Panel_txtProveedor.Name = "Panel_txtProveedor";
+            this.Panel_txtProveedor.Size = new System.Drawing.Size(1106, 44);
+            this.Panel_txtProveedor.TabIndex = 4;
+            // 
+            // Label_Proveedores
+            // 
+            this.Label_Proveedores.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label_Proveedores.AutoSize = true;
+            this.Label_Proveedores.ForeColor = System.Drawing.SystemColors.Window;
+            this.Label_Proveedores.Location = new System.Drawing.Point(451, 4);
+            this.Label_Proveedores.Name = "Label_Proveedores";
+            this.Label_Proveedores.Size = new System.Drawing.Size(200, 37);
+            this.Label_Proveedores.TabIndex = 2;
+            this.Label_Proveedores.Text = "Proveedores";
+            // 
+            // btn_WF_CerrarProveedor
+            // 
+            this.btn_WF_CerrarProveedor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_WF_CerrarProveedor.BackColor = System.Drawing.Color.Navy;
+            this.btn_WF_CerrarProveedor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_WF_CerrarProveedor.FlatAppearance.BorderSize = 0;
+            this.btn_WF_CerrarProveedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_WF_CerrarProveedor.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_WF_CerrarProveedor.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btn_WF_CerrarProveedor.Location = new System.Drawing.Point(1, 0);
+            this.btn_WF_CerrarProveedor.Name = "btn_WF_CerrarProveedor";
+            this.btn_WF_CerrarProveedor.Size = new System.Drawing.Size(51, 44);
+            this.btn_WF_CerrarProveedor.TabIndex = 1;
+            this.btn_WF_CerrarProveedor.Text = "X";
+            this.btn_WF_CerrarProveedor.UseVisualStyleBackColor = false;
+            this.btn_WF_CerrarProveedor.Click += new System.EventHandler(this.btn_WF_CerrarProveedor_Click);
+            // 
+            // btn_WF_Eliminar
+            // 
+            this.btn_WF_Eliminar.BackColor = System.Drawing.Color.Navy;
+            this.btn_WF_Eliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_WF_Eliminar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_WF_Eliminar.FlatAppearance.BorderSize = 2;
+            this.btn_WF_Eliminar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btn_WF_Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_WF_Eliminar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_WF_Eliminar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_WF_Eliminar.Location = new System.Drawing.Point(51, 211);
+            this.btn_WF_Eliminar.Name = "btn_WF_Eliminar";
+            this.btn_WF_Eliminar.Size = new System.Drawing.Size(149, 46);
+            this.btn_WF_Eliminar.TabIndex = 5;
+            this.btn_WF_Eliminar.Text = "Eliminar";
+            this.btn_WF_Eliminar.UseVisualStyleBackColor = false;
+            this.btn_WF_Eliminar.Click += new System.EventHandler(this.btn_WF_Eliminar_Click);
+            // 
+            // btn_WF_Actualizar
+            // 
+            this.btn_WF_Actualizar.BackColor = System.Drawing.Color.Navy;
+            this.btn_WF_Actualizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_WF_Actualizar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_WF_Actualizar.FlatAppearance.BorderSize = 2;
+            this.btn_WF_Actualizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btn_WF_Actualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_WF_Actualizar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_WF_Actualizar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_WF_Actualizar.Location = new System.Drawing.Point(51, 283);
+            this.btn_WF_Actualizar.Name = "btn_WF_Actualizar";
+            this.btn_WF_Actualizar.Size = new System.Drawing.Size(149, 46);
+            this.btn_WF_Actualizar.TabIndex = 6;
+            this.btn_WF_Actualizar.Text = "Actualizar";
+            this.btn_WF_Actualizar.UseVisualStyleBackColor = false;
+            // 
+            // btn_WF_Agregar
+            // 
+            this.btn_WF_Agregar.BackColor = System.Drawing.Color.Navy;
+            this.btn_WF_Agregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_WF_Agregar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_WF_Agregar.FlatAppearance.BorderSize = 2;
+            this.btn_WF_Agregar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btn_WF_Agregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_WF_Agregar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_WF_Agregar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_WF_Agregar.Location = new System.Drawing.Point(51, 356);
+            this.btn_WF_Agregar.Name = "btn_WF_Agregar";
+            this.btn_WF_Agregar.Size = new System.Drawing.Size(149, 46);
+            this.btn_WF_Agregar.TabIndex = 7;
+            this.btn_WF_Agregar.Text = "Agregar";
+            this.btn_WF_Agregar.UseVisualStyleBackColor = false;
+            // 
+            // btn_WF_Buscar
+            // 
+            this.btn_WF_Buscar.BackColor = System.Drawing.Color.Navy;
+            this.btn_WF_Buscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_WF_Buscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_WF_Buscar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_WF_Buscar.FlatAppearance.BorderSize = 2;
+            this.btn_WF_Buscar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btn_WF_Buscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_WF_Buscar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_WF_Buscar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_WF_Buscar.Location = new System.Drawing.Point(527, 29);
+            this.btn_WF_Buscar.Name = "btn_WF_Buscar";
+            this.btn_WF_Buscar.Size = new System.Drawing.Size(149, 48);
+            this.btn_WF_Buscar.TabIndex = 8;
+            this.btn_WF_Buscar.Text = "Buscar";
+            this.btn_WF_Buscar.UseVisualStyleBackColor = false;
+            // 
+            // Panel_WF_Controles_Proveedor
+            // 
+            this.Panel_WF_Controles_Proveedor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel_WF_Controles_Proveedor.BackColor = System.Drawing.Color.Snow;
+            this.Panel_WF_Controles_Proveedor.Controls.Add(this.PicBox_WF_Proveedor);
+            this.Panel_WF_Controles_Proveedor.Controls.Add(this.btn_WF_Actualizar);
+            this.Panel_WF_Controles_Proveedor.Controls.Add(this.btn_WF_Agregar);
+            this.Panel_WF_Controles_Proveedor.Controls.Add(this.btn_WF_Eliminar);
+            this.Panel_WF_Controles_Proveedor.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Panel_WF_Controles_Proveedor.Location = new System.Drawing.Point(857, 44);
+            this.Panel_WF_Controles_Proveedor.Name = "Panel_WF_Controles_Proveedor";
+            this.Panel_WF_Controles_Proveedor.Size = new System.Drawing.Size(249, 479);
+            this.Panel_WF_Controles_Proveedor.TabIndex = 9;
+            this.Panel_WF_Controles_Proveedor.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_WF_Controles_Proveedor_Paint);
+            // 
+            // PicBox_WF_Proveedor
+            // 
+            this.PicBox_WF_Proveedor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicBox_WF_Proveedor.BackColor = System.Drawing.Color.Snow;
+            this.PicBox_WF_Proveedor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PicBox_WF_Proveedor.Image = ((System.Drawing.Image)(resources.GetObject("PicBox_WF_Proveedor.Image")));
+            this.PicBox_WF_Proveedor.Location = new System.Drawing.Point(25, 29);
+            this.PicBox_WF_Proveedor.Name = "PicBox_WF_Proveedor";
+            this.PicBox_WF_Proveedor.Size = new System.Drawing.Size(200, 125);
+            this.PicBox_WF_Proveedor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicBox_WF_Proveedor.TabIndex = 8;
+            this.PicBox_WF_Proveedor.TabStop = false;
+            // 
+            // Panel_WF_Info_Proveedor
+            // 
+            this.Panel_WF_Info_Proveedor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel_WF_Info_Proveedor.BackColor = System.Drawing.Color.Snow;
+            this.Panel_WF_Info_Proveedor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Panel_WF_Info_Proveedor.Controls.Add(this.btn_WF_Buscar);
+            this.Panel_WF_Info_Proveedor.Controls.Add(this.txt_Busqueda_Proveedor);
+            this.Panel_WF_Info_Proveedor.Controls.Add(this.DGV_WF_Proveedor);
+            this.Panel_WF_Info_Proveedor.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Panel_WF_Info_Proveedor.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.Panel_WF_Info_Proveedor.Location = new System.Drawing.Point(0, 44);
+            this.Panel_WF_Info_Proveedor.Name = "Panel_WF_Info_Proveedor";
+            this.Panel_WF_Info_Proveedor.Size = new System.Drawing.Size(858, 479);
+            this.Panel_WF_Info_Proveedor.TabIndex = 10;
+            // 
+            // txt_Busqueda_Proveedor
+            // 
+            this.txt_Busqueda_Proveedor.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txt_Busqueda_Proveedor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Busqueda_Proveedor.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_Busqueda_Proveedor.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Busqueda_Proveedor.Location = new System.Drawing.Point(163, 34);
+            this.txt_Busqueda_Proveedor.Multiline = true;
+            this.txt_Busqueda_Proveedor.Name = "txt_Busqueda_Proveedor";
+            this.txt_Busqueda_Proveedor.Size = new System.Drawing.Size(323, 38);
+            this.txt_Busqueda_Proveedor.TabIndex = 1;
+            // 
+            // DGV_WF_Proveedor
+            // 
+            this.DGV_WF_Proveedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_WF_Proveedor.Location = new System.Drawing.Point(22, 95);
+            this.DGV_WF_Proveedor.Name = "DGV_WF_Proveedor";
+            this.DGV_WF_Proveedor.RowHeadersWidth = 51;
+            this.DGV_WF_Proveedor.RowTemplate.Height = 24;
+            this.DGV_WF_Proveedor.Size = new System.Drawing.Size(815, 359);
+            this.DGV_WF_Proveedor.TabIndex = 0;
+            this.DGV_WF_Proveedor.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_WF_Proveedor_CellContentClick);
+            // 
+            // WF_Proveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 523);
+            this.Controls.Add(this.Panel_WF_Info_Proveedor);
+            this.Controls.Add(this.Panel_WF_Controles_Proveedor);
+            this.Controls.Add(this.Panel_txtProveedor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WF_Proveedor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WF_Proveedor";
+            this.Load += new System.EventHandler(this.WF_Proveedor_Load);
+            this.Panel_txtProveedor.ResumeLayout(false);
+            this.Panel_txtProveedor.PerformLayout();
+            this.Panel_WF_Controles_Proveedor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_WF_Proveedor)).EndInit();
+            this.Panel_WF_Info_Proveedor.ResumeLayout(false);
+            this.Panel_WF_Info_Proveedor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_WF_Proveedor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel Panel_txtProveedor;
+        private System.Windows.Forms.Button btn_WF_Eliminar;
+        private System.Windows.Forms.Button btn_WF_Actualizar;
+        private System.Windows.Forms.Button btn_WF_Agregar;
+        private System.Windows.Forms.Button btn_WF_Buscar;
+        private System.Windows.Forms.Panel Panel_WF_Controles_Proveedor;
+        private System.Windows.Forms.Panel Panel_WF_Info_Proveedor;
+        private System.Windows.Forms.DataGridView DGV_WF_Proveedor;
+        private System.Windows.Forms.Button btn_WF_CerrarProveedor;
+        private System.Windows.Forms.TextBox txt_Busqueda_Proveedor;
+        private System.Windows.Forms.PictureBox PicBox_WF_Proveedor;
+        private System.Windows.Forms.Label Label_Proveedores;
+    }
+}
