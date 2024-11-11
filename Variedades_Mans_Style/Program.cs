4872f954f029063ad84268dafd33dfff1b839a23
DevSaile <jeserelias12@gmail.com>
2024-11-10 20:04:59 -0600

    Revert "Revert "pull a cambio de urbina"" Resolviendo conflictos This reverts commit a62b16c0c92e5278b34f7e270450ca99817dc295.

--- Variedades_Mans_Style/Program.cs
+++ Variedades_Mans_Style/Program.cs
@@ -16,7 +16,7 @@ namespace Variedades_Man_s_Style
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AgregarProveedor());
         }
     }
 }
